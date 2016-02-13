68532ae5d7ab9b4625503dd47bbd3243ba4ff41b
Oscar L. aka moas <lasm.landry@gmail.com>
2016-02-13 01:49:46 +0100

    raise an importerror exception if msgpack not installed

--- rpcviaredis/transport.py
+++ rpcviaredis/transport.py
@@ -53,34 +53,31 @@ class PickleTransport(AbstractTransport):
 try:
     import msgpack
 except ImportError as exc:
-    import warnings
 
-    class FakeMsgPack:
-        _fallback = JsonTransport()
-        warnings.warn("{}: Msgpack not found. JSON transport is fallback".format(exc), category=ImportWarning)
+    class MsgPackTransport(AbstractTransport):
 
-        def packb(self, data):
-            return self._fallback.packed(data)
+        def packed(self, data):
+            raise ImportError("msgpack package not found")
 
-        def unpackb(self, data):
-            return self._fallback.unpacked(data)
-    msgpack = FakeMsgPack()
+        def unpacked(self, data):
+            raise ImportError("msgpack package not found")
 
+else:
 
-class MsgPackTransport(AbstractTransport):
+    class MsgPackTransport(AbstractTransport):
 
-    def packed(self, data):
-        try:
-            return msgpack.packb(data)
-        except msgpack.exceptions.PackException as exc:
-            raise PackedException("Exception {}: impossible to packed data".format(exc.__class__.__name__))
+        def packed(self, data):
+            try:
+                return msgpack.packb(data)
+            except msgpack.exceptions.PackException as exc:
+                raise PackedException("Exception {}: impossible to packed data".format(exc.__class__.__name__))
 
-    def unpacked(self, data):
-        data = data.encode() if isinstance(data, str) else data
-        try:
-            retval = msgpack.unpackb(data)
-        except msgpack.exceptions.UnpackException as exc:
-            raise UnpackedException("Exception {}: impossible to unpacked data".format(exc.__class__.__name__))
+        def unpacked(self, data):
+            data = data.encode() if isinstance(data, str) else data
+            try:
+                retval = msgpack.unpackb(data)
+            except msgpack.exceptions.UnpackException as exc:
+                raise UnpackedException("Exception {}: impossible to unpacked data".format(exc.__class__.__name__))
 
-        retval = {k if isinstance(k, str) else k.decode(): v for k, v in retval.items()}
-        return retval
+            retval = {k if isinstance(k, str) else k.decode(): v for k, v in retval.items()}
+            return retval
